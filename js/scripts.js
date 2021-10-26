266b45a56a641fb2bd8d095defacdc1b0a459b91
Liam Eller <liamthelastson@gmail.com>
2021-10-26 13:31:03 -0700

    Add html for ui logic and javascript for our function.
    co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// Businesses logic
+// Business Logic
 
 function wordCounter(text) {
   if (text.trim().length === 0) {
@@ -13,3 +13,31 @@ function wordCounter(text) {
   });
   return wordCount;
 }
+
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount++;
+    }
+  });
+  return wordCount;
+}
+
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
