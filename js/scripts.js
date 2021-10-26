857f58fe7bda9aba0b25d74433265cc3caf45bea
Liam Eller <liamthelastson@gmail.com>
2021-10-26 11:09:20 -0700

    Add wordCounter function to scripts.sj
    co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Businesses logic
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(word) {
+    wordCount++;
+  });
+  return wordCount;
+}
