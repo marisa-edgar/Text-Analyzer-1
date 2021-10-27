8bd7d08c4eff6c99e6d1112cf514dc8f58bd5a0c
Liam Eller <liamthelastson@gmail.com>
2021-10-26 17:43:35 -0700

    work in progress.
    co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -24,19 +24,19 @@ function wordCounter(text) {
 
 // numberOfOccurrencesInText()
 
-function numberOfOccurrencesInText(word, text) {
-  if (noInputtedWord(word, text)) {
-    return 0;
-  }
-  const wordArray = text.split(" ");
-  let wordCount = 0;
-  wordArray.forEach(function(element) {
-    if (element.toLowerCase().includes(word.toLowerCase())) {
-      wordCount++;
-    }
-  });
-  return wordCount;
-}
+// function numberOfOccurrencesInText(word, text) {
+//   if (noInputtedWord(word, text)) {
+//     return 0;
+//   }
+//   const wordArray = text.split(" ");
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (element.toLowerCase().includes(word.toLowerCase())) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
 
 // boldPassage()
 
@@ -59,19 +59,59 @@ function boldPassage(word, text) {
   return htmlString + "</p>";
 }
 
+
+// Beyond this point the code will be our own
+
+// numberOFOccurrencesOfWOrd()
+
+function numberOFOccurrencesOfWords (input1, input2, input3, text) {
+  if (noInputtedWord(input1, input2, input3, text)) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element) {
+    if (element.toLowerCase().includes(input1.toLowerCase())) {
+      wordCount++;
+    } else if (element.toLowerCase().includes(input2.toLowerCase())) {
+      wordCount++;
+    } else if (element.toLowerCase().includes(input3.toLowerCase())) {
+      wordCount++;
+    } 
+  });
+  return wordCount;
+}
+
 // UI Logic
+
+// $(document).ready(function(){
+//   $("form#word-counter").submit(function(event){
+//     event.preventDefault();
+//     const passage = $("#text-passage").val();
+//     const word = $("#word").val();
+//     const wordCount = wordCounter(passage);
+//     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+//     $("#total-count").html(wordCount);
+//     $("#selected-count").html(occurrencesOfWord);
+//     $("#bolded-passage").html(boldPassage(word, passage));
+//   });
+// });
+
+// Beyond this point the code will be our own
 
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
     event.preventDefault();
     const passage = $("#text-passage").val();
-    const word = $("#word").val();
+    const input1 = $("#input1").val();
+    const input2 = $("#input2").val();
+    const input3 = $("#input3").val();
     const wordCount = wordCounter(passage);
-    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    const occurrencesOfWord = numberOFOccurrencesOfWords(input1, input2, input3, passage);
     $("#total-count").html(wordCount);
-    $("#selected-count").html(occurrencesOfWord);
-    $("#bolded-passage").html(boldPassage(word, passage));
+    $("#output1").html(occurrencesOfWord);
+    $("#output2").html(occurrencesOfWord);
+    $("#output3").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(input1, input2, input3, passage));
   });
 });
-
-// Beyond this point the code will be our own
